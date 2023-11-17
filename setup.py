c1cb43034ecf91b026ede9fc5f464ffd5fe4bfc9
Apinya <apinyarr@hotmail.com>
2023-11-17 12:31:17 -0500

    debug

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
-with open('./requirement.txt') as f:
-    requirements = f.read().splitlines()
+# with open('./requirement.txt') as f:
+#     requirements = f.read().splitlines()
 
 setup(
     name="foo",
     version="1.0",
     packages=find_packages(),
-    install_requires=requirements,
+    # install_requires=requirements,
+    install_requires=[
+        'flask == 3.0.0',
+        'matplotlib == 3.7.3',
+        'pymongo == 4.5.0'
+    ],
 )
