0dbc3a90ad80474ea5eedc979fac38b48eb80ae6
Apinya <apinyarr@hotmail.com>
2023-11-19 20:31:18 -0500

    delete unused file

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker exec mongodb mongosh --eval 'db.wordscollection.find({ "xmin": 150,"xmax": 200,"ymin": 150,"ymax": 200 })\' dictionary
-docker exec mongodb mongosh --eval 'db.wordscollection.find({ "xmin": 150,"xmax": 200,"ymin": 150,"ymax": 200 })' dictionary
